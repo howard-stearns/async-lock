4878448250aa181b33e1004e6b22e610517ed8c2
Howard Stearns <howard@highfidelity.io>
2015-08-31 21:31:03 -0700

    Change license from CC BY-SA 1.0, to MIT, because I want to stay within the Open Source Initiative list.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
-/*jslint node: true, ass: true */
+/*jslint node: true, vars: true, ass: true*/
+/*Copyright (c) 2013-2015 Howard Stearns. MIT License*/
 
 // This improves performance when there are a lot of locks pending on the same key.
 var Queue = exports.Queue = require('./queue.js');
@@ -10,35 +11,35 @@ function noop () {}
 // Execute iterator(unlock) while no other asynchronous proc has locked the same key.
 // iterator can be asynchronous, but it must call unlock() to release key when ready (e.g., in its own callback), regardless of error.
 var locks = {}, lockMultiple;
-function lock(key, iterator) {
-	if (!key) { return iterator(noop); } // As a convenience, if key is falsey, don't actually lock.
-	if (util.isArray(key)) { return lockMultiple(key, iterator); }
-	var pending = locks[key];
-	if (pending) {
-		pending.enqueue(iterator); //push
-	} else {
-		pending = locks[key] = new Queue(); //[]
-		var unlock = function () {
-			if (!pending.isEmpty()) { //!length
-				var cont = pending.dequeue(); //shift, but faster
-				setImmediate(function () { cont(unlock); });
-			} else {
-				delete locks[key];
-			}
-		};
-		iterator(unlock);
-	}
+function lock(key, critical) {
+    if (!key) { return critical(noop); } // As a convenience, if key is falsey, don't actually lock.
+    if (util.isArray(key)) { return lockMultiple(key, critical); }
+    var pending = locks[key];
+    if (pending) {
+        pending.enqueue(critical); //push
+    } else {
+        pending = locks[key] = new Queue(); //[]
+        var unlock = function () {
+            if (!pending.isEmpty()) { //!length
+                var cont = pending.dequeue(); //shift, but faster
+                setImmediate(function () { cont(unlock); });
+            } else {
+                delete locks[key];
+            }
+        };
+        critical(unlock);
+    }
 }
 
-// Lock a number of keys at once, freeing them when all iterator(unlock) calls unlock.
+// Lock a number of keys at once, freeing them when all critical(unlock) calls unlock.
 // keys must not have duplciates!
-function lockMultiple(keys, iterator) {   // Iterator is run only in a context in which the locks for all keys are closed for our use.
-	async.map(keys, function (key, lcb) { // Acquire a lock on each key and accumulate the unlock functions.
-		lock(key, function (unlock) { lcb(null, unlock); });
-	}, function (e, pendingLocks) {       // It is now our turn. Every lock iterator has been called, but we haven't unlocked anything yet.
-		noop(e);                          // never set, by construction
-		iterator(function () { pendingLocks.forEach(function (unlock) { unlock(); }); });
-	});
+function lockMultiple(keys, critical) {   // Critical is run only in a context in which the locks for all keys are closed for our use.
+    async.map(keys, function (key, lcb) { // Acquire a lock on each key and accumulate the unlock functions.
+        lock(key, function (unlock) { lcb(null, unlock); });
+    }, function (e, pendingLocks) {       // It is now our turn. Every lock critical has been called, but we haven't unlocked anything yet.
+        noop(e);                          // never set, by construction
+        critical(function () { pendingLocks.forEach(function (unlock) { unlock(); }); });
+    });
 }
 exports.lock = lock;
 exports.lockMultiple = util.deprecate(lockMultiple, "lockMultiple is deprecated: Use lock with an array first argument instead.");
--- test/test.js
+++ test/test.js
@@ -5,28 +5,29 @@ var mocha = require('mocha'), describe = mocha.describe, it = mocha.it;
 var lock = require('../index').lock;
 
 describe('async-lock', function () {
-	it('locked inner requires outer unlock', function (done) {
-		var outerUnlocked = false;
-		lock('test', function (unlockOuter) {
-			lock('test', function (unlock) { assert.ok(outerUnlocked); unlock(); done(); });
-			outerUnlocked = true;
-			unlockOuter();
-		});
-	});
-	it('accepts falsey key without actually locking', function (done) {
-		var outerUnlocked = false;
-		lock('', function (unlockOuter) { // Note !outerUnlocked assertion.
-			lock('', function (unlock) { assert.ok(!outerUnlocked); unlock(); done(); });
-			outerUnlocked = true;
-			unlockOuter();
-		});
-	});
-	it('accepts arrays of keys', function (done) {
-		var outerUnlocked = false;
-		lock(['a', 'b', 'c'], function (unlockOuter) {
-			lock('b', function (unlock) { assert.ok(outerUnlocked); unlock(); done(); });
-			outerUnlocked = true;
-			unlockOuter();
-		});
-	});
+    it('locked inner requires outer unlock', function (done) {
+        var outerUnlocked = false;
+        lock('test', function (unlockOuter) {
+            lock('test', function (unlock) { assert.ok(outerUnlocked); unlock(); done(); });
+            outerUnlocked = true;
+            unlockOuter();
+        });
+    });
+    it('accepts falsey key without actually locking', function (done) {
+        var outerUnlocked = false;
+        lock('', function (unlockOuter) { // Note !outerUnlocked assertion.
+            lock('', function (unlock) { assert.ok(!outerUnlocked); unlock(); done(); });
+            outerUnlocked = true;
+            unlockOuter();
+        });
+    });
+    it('accepts arrays of keys', function (done) {
+        var outerUnlocked = false;
+        lock(['a', 'b', 'c'], function (unlockOuter) {
+            lock('b', function (unlock) { assert.ok(outerUnlocked); unlock(); done(); });
+            outerUnlocked = true;
+            unlockOuter();
+        });
+    });
+    it.skip('can have two different modules lock the same resource in the same process');
 });
